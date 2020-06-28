74058e61266a4fdeb427a7b9333a75a6580e3739
Song-Hyunsung <song.hyunsung96@gmail.com>
2020-06-28 18:18:55 -0400

    impl send message and show message

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
+	messageInput: String = "";
 
-  constructor() { }
+  constructor(public chatService: ChatService) { }
 
   ngOnInit(): void {
+  }
+
+  sendMessage(): void {
+  	if(this.messageInput){
+  		this.chatService.sendMessage(this.messageInput);
+  	}
+
+  	this.messageInput = "";
+  }
+
+  showAll(): void {
+ 		console.log(this.chatService.messageArray);
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -13,37 +13,54 @@ export class ChatService {
 	WEBSOCKET_SERVER_URL: String = "http://localhost:7000/ws";
 	// Variable for stomp client
 	stompClient: any;
+  username: String;
+  // Temporary in-memory array to store messages
+  messageArray: String[] = [];
 
   constructor(private router: Router){ }
 
   establishWebSocketConnection(username: String): void {
   	let socket = new SockJS(this.WEBSOCKET_SERVER_URL);
   	this.stompClient = Stomp.over(socket);
+    this.username = username;
 
   	// Connect to stomp client, provide anonymous onConnect callback function and onError callback function
-  	this.stompClient.connect({}, ()=>{
-	  	this.stompClient.subscribe("/topic/public", this.onMessageReceived);
-	  	this.stompClient.send("/app/chat.addUser", {}, JSON.stringify({ sender: username, type: "JOIN" }));
-      this.router.navigate(['/room']);
-  	}, this.onError);
+  	this.stompClient.connect({}, this.onConnect.bind(this, username), this.onError);
+  }
+
+  onConnect(username: String): void {
+    this.stompClient.subscribe("/topic/public", this.onMessageReceived.bind(this));
+    this.stompClient.send("/app/chat.addUser", {}, JSON.stringify({ sender: username, type: "JOIN" }));
+    this.router.navigate(['/room']);
   }
 
   onError(): void {
     alert("Service is not available at this moment, Please try again later");
     console.log("onError method in ChatService invoked");
     this.router.navigate(["/"]);
-
   }
 
   onMessageReceived(payload: any): void {
   	let message: any = JSON.parse(payload.body);
 
   	if(message.type === "JOIN"){
-  		console.log("ON MESSAGE RECEIVED: USER JOINED");
+      this.messageArray.push(message.sender + " joined the room");
   	} else if(message.type === "LEAVE"){
-  		console.log("ON MESSAGE RECEIVED: USER LEFT");
+      this.messageArray.push(message.sender + " left the room");
   	} else {
-  		console.log("ON MESSAGE RECEIVED: USER SENT MESSAGE");
+      this.messageArray.push(message.sender + ": " + message.content);
   	}
+  }
+
+  sendMessage(message: String): void {
+    if(message && this.stompClient){
+      let chatMessage = {
+        sender: this.username,
+        content: message,
+        type: "CHAT"
+      };
+
+      this.stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+    }
   }
 }
